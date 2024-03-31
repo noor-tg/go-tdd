2d48f88524af836bac4712fa4da148497b494d35
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-03-31 21:58:26 +0200

    feat(hello): learn about function args, string compare with conditions , subtests, testing interface, testing helper methods

--- hello.go
+++ hello.go
@@ -3,5 +3,8 @@ package main
 const greetingPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return greetingPrefix + name
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("noor")
-	want := "Hello, noor"
+	t.Run("hello to people", func(t *testing.T) {
+		got := Hello("noor")
+		want := "Hello, noor"
 
+		assertStrEqual(t, got, want)
+	})
+	t.Run("say 'hello, world' when name is empty", func(t *testing.T) {
+
+		got := Hello("")
+		want := "Hello, World"
+
+		assertStrEqual(t, got, want)
+	})
+}
+
+func assertStrEqual(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
