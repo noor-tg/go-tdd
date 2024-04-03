aad1d693d4567a6a595aa3862554f82fbdb9f7c5
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-03 19:39:54 +0200

    feat(structs): start to learn about structs
    - calc perimeter and area  for rectangle

---
+++ structs/math.go
@@ -0,0 +1,9 @@
+package structs
+
+func Perimeter(width float64, height float64) float64 {
+	return (width + height) * 2
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
