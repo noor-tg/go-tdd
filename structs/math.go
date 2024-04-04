d87c5766aacd0ee639e289ad1413339cbff12b8b
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-04 15:52:58 +0200

    feat(structs): calc area for triangle
    - learn about show full type info with value

--- structs/math.go
+++ structs/math.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rect Rectangle) float64 {
 	return (rect.Width + rect.Height) * 2
 }
@@ -20,4 +25,7 @@ func (r Rectangle) Area() float64 {
 }
 func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
+}
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height * .5
 }
--- structs/math_test.go
+++ structs/math_test.go
@@ -30,13 +30,14 @@ func TestArea(t *testing.T) {
 	areaTests := []Sample{ // the test data
 		{Rectangle{12, 6}, 72.0},
 		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 30.0},
 	}
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
 
 		if got != want {
-			t.Errorf("got %g want %g", got, want)
+			t.Errorf("%#v got %g want %g", shape, got, want)
 		}
 	}
 
