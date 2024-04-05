bb8d7fd06c07adb3ffb47f800b0e92129a1b05bf
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-05 15:39:22 +0200

    test(maps): move some logic out of assertStrings to make it more usable

--- maps/dectionary_test.go
+++ maps/dectionary_test.go
@@ -6,12 +6,12 @@ func TestSearch(t *testing.T) {
 	dectionary := map[string]string{"test": "this is just a test"}
 
 	got := Search(dectionary, "test")
-	assertStrings(t, got)
+	want := "this is just a test"
+	assertStrings(t, got, want)
 }
 
-func assertStrings(t testing.TB, got string) {
-	want := "this is just a test"
-
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given, %q", got, want, "test")
 	}
