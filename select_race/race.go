5ba4adb603df0b224638ea368fdca09535423625
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-08 01:47:46 +0200

    feat(select): start to learn about select
    - learn about httptest module
    - learn about defer operator
    - learn about time module funcs
    - learn about benifits of mocking with external services

---
+++ select_race/race.go
@@ -0,0 +1,25 @@
+package select_race
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) string {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startA := time.Now()
+	http.Get(url)
+	aDuration := time.Since(startA)
+
+	return aDuration
+}
