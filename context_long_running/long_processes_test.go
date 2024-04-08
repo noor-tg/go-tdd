490ee3215be140df193beae90fa7ea31c92dbfeb
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-09 01:35:32 +0200

    feat(context): start learn about context

---
+++ context_long_running/long_processes_test.go
@@ -0,0 +1,30 @@
+package context_long_running
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "Hello, World"
+	srv := Server(&StubStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	srv.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	}
+
+}
