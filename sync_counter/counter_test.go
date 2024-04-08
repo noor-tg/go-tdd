a084d91aeb1aa60c77135144482213154eeeebbd
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-09 01:35:12 +0200

    refactor(sync): make counter Async safe with mutex

--- sync_counter/counter_test.go
+++ sync_counter/counter_test.go
@@ -1,19 +1,29 @@
 package sync_counter
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("increment counter 3 times  leave it at 3", func(t *testing.T) {
+		wantedCount := 1000
 		counter := Counter{}
-		counter.Inc()
-		counter.Inc()
-		counter.Inc()
-		assertCounter(t, counter, 3)
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, &counter, 1000)
 	})
 
 }
 
-func assertCounter(t testing.TB, counter Counter, want int) {
+func assertCounter(t testing.TB, counter *Counter, want int) {
 	t.Helper()
 	got := counter.Value()
 
