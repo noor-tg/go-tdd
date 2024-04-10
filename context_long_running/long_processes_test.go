6f7a7fb6dbe0ea89d5cedee98830de454f3e9cff
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-10 18:16:44 +0200

    feat(context): cancel outside process when request is canceled after time
    - learn about context
    - how to cancel context after some time with AfterFunc
    - how to listen for fetched outside process data or cancel request

--- context_long_running/long_processes_test.go
+++ context_long_running/long_processes_test.go
@@ -1,30 +1,75 @@
 package context_long_running
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
+type SpyStore struct {
 	response string
+	canceled bool
+	t        testing.TB
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
-func TestServer(t *testing.T) {
-	data := "Hello, World"
-	srv := Server(&StubStore{data})
+func (s *SpyStore) Cancel() {
+	s.canceled = true
+}
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
-
-	srv.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+func (s *SpyStore) assertNotCanceled() {
+	s.t.Helper()
+	if s.canceled {
+		s.t.Error("it should not have canceled the store")
 	}
+}
 
+func (s *SpyStore) assertCanceled() {
+	s.t.Helper()
+	if !s.canceled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func TestServer(t *testing.T) {
+	t.Run("make request to store normally", func(t *testing.T) {
+		data := "Hello, World"
+		store := SpyStore{data, false, t}
+		srv := Server(&store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+
+		store.assertNotCanceled()
+	})
+
+	t.Run("stop background process if request canceled by user", func(t *testing.T) {
+		data := "Hello, World"
+		store := SpyStore{data, false, t}
+		srv := Server(&store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancelingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancelingCtx)
+
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		if !store.canceled {
+		}
+	})
 }
