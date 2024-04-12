0b8a2640b8d5ce9a3fbb579f56b4cfb9bbfeb44a
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-12 16:55:30 +0200

    refactor(pbt): use switch case with loop to convert to roman numbers tested to 50
    - start from the arabic number for loop
    - substract the larger noun number in roman numerials (x = 10)
    - make special cases for special numbers like 4, 9
    - repeat 'I' for 3 or less number in loop

--- pbt/to_romans.go
+++ pbt/to_romans.go
@@ -4,18 +4,28 @@ import "strings"
 
 func ConvertToRoman(number int) string {
 	var result strings.Builder
-	for i := number; i > 0; i-- {
-		if i == 4 {
-			result.WriteString("IV")
-			break
-		} else if i == 5 {
+	for number > 0 {
+		switch {
+		case number > 9:
+			result.WriteString("X")
+			number -= 10
+
+		case number > 8:
+			result.WriteString("IX")
+			number -= 9
+
+		case number > 4:
 			result.WriteString("V")
-			break
-		} else if i == 6 {
-			result.WriteString("VI")
-			break
+			number -= 5
+
+		case number > 3:
+			result.WriteString("IV")
+			number -= 4
+
+		default:
+			result.WriteString("I")
+			number--
 		}
-		result.WriteString("I")
 	}
 	return result.String()
 }
--- pbt/to_romans_test.go
+++ pbt/to_romans_test.go
@@ -38,6 +38,96 @@ func TestRomanNumerals(t *testing.T) {
 			input: 6,
 			want:  "VI",
 		},
+		{
+			desc:  "7 converted to VII",
+			input: 7,
+			want:  "VII",
+		},
+		{
+			desc:  "8 converted to VIII",
+			input: 8,
+			want:  "VIII",
+		},
+		{
+			desc:  "9 converted to IX",
+			input: 9,
+			want:  "IX",
+		},
+		{
+			desc:  "10 converted to X",
+			input: 10,
+			want:  "X",
+		},
+		{
+			desc:  "11 converted to XI",
+			input: 11,
+			want:  "XI",
+		},
+		{
+			desc:  "12 converted to XII",
+			input: 12,
+			want:  "XII",
+		},
+		{
+			desc:  "13 converted to XIII",
+			input: 13,
+			want:  "XIII",
+		},
+		{
+			desc:  "14 converted to XIV",
+			input: 14,
+			want:  "XIV",
+		},
+		{
+			desc:  "15 converted to XV",
+			input: 15,
+			want:  "XV",
+		},
+		{
+			desc:  "16 converted to XVI",
+			input: 16,
+			want:  "XVI",
+		},
+		{
+			desc:  "17 converted to XVII",
+			input: 17,
+			want:  "XVII",
+		},
+		{
+			desc:  "18 converted to XVIII",
+			input: 18,
+			want:  "XVIII",
+		},
+		{
+			desc:  "19 converted to XIX",
+			input: 19,
+			want:  "XIX",
+		},
+		{
+			desc:  "20 converted to XX",
+			input: 20,
+			want:  "XX",
+		},
+		{
+			desc:  "29 converted to XXIX",
+			input: 29,
+			want:  "XXIX",
+		},
+		{
+			desc:  "30 converted to XXX",
+			input: 30,
+			want:  "XXX",
+		},
+		{
+			desc:  "39 converted to XXXIX",
+			input: 39,
+			want:  "XXXIX",
+		},
+		{
+			desc:  "40 converted to XXXX",
+			input: 40,
+			want:  "XXXX",
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
