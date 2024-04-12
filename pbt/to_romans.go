692d30df9cf849a779f44ffb1d189fca04ffb7ef
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-12 17:58:42 +0200

    refactor(pbt): convert repeated alg to data based
    - set cases to symboles in structure {value, symbol}
    - set roman known cases to array of structure
    - change tests to check symbols to symobles for 1984
    - subtract as needed when number larger or equal than symbol value

--- pbt/to_romans.go
+++ pbt/to_romans.go
@@ -2,29 +2,38 @@ package pbt
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+// roman numbers based on symbol
+var AllRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(number int) string {
 	var result strings.Builder
-	for number > 0 {
-		switch {
-		case number > 9:
-			result.WriteString("X")
-			number -= 10
-
-		case number > 8:
-			result.WriteString("IX")
-			number -= 9
-
-		case number > 4:
-			result.WriteString("V")
-			number -= 5
-
-		case number > 3:
-			result.WriteString("IV")
-			number -= 4
-
-		default:
-			result.WriteString("I")
-			number--
+	// iterate throw roman numbers
+	for _, numeral := range AllRomanNumerals {
+		// if my current number is equal or larger than this number
+		for number >= numeral.Value {
+			// write the symbol
+			result.WriteString(numeral.Symbol)
+			// subtract the value
+			number -= numeral.Value
 		}
 	}
 	return result.String()
--- pbt/to_romans_test.go
+++ pbt/to_romans_test.go
@@ -1,136 +1,162 @@
 package pbt
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	testCases := []struct {
-		desc  string
 		input int
 		want  string
 	}{
 		{
-			desc:  "1 converted to I",
 			input: 1,
 			want:  "I",
 		},
 		{
-			desc:  "2 converted to II",
 			input: 2,
 			want:  "II",
 		},
 		{
-			desc:  "3 converted to III",
 			input: 3,
 			want:  "III",
 		},
 		{
-			desc:  "4 converted to IV",
 			input: 4,
 			want:  "IV",
 		},
 		{
-			desc:  "5 converted to V",
 			input: 5,
 			want:  "V",
 		},
 		{
-			desc:  "6 converted to VI",
 			input: 6,
 			want:  "VI",
 		},
 		{
-			desc:  "7 converted to VII",
 			input: 7,
 			want:  "VII",
 		},
 		{
-			desc:  "8 converted to VIII",
 			input: 8,
 			want:  "VIII",
 		},
 		{
-			desc:  "9 converted to IX",
 			input: 9,
 			want:  "IX",
 		},
 		{
-			desc:  "10 converted to X",
 			input: 10,
 			want:  "X",
 		},
 		{
-			desc:  "11 converted to XI",
 			input: 11,
 			want:  "XI",
 		},
 		{
-			desc:  "12 converted to XII",
 			input: 12,
 			want:  "XII",
 		},
 		{
-			desc:  "13 converted to XIII",
 			input: 13,
 			want:  "XIII",
 		},
 		{
-			desc:  "14 converted to XIV",
 			input: 14,
 			want:  "XIV",
 		},
 		{
-			desc:  "15 converted to XV",
 			input: 15,
 			want:  "XV",
 		},
 		{
-			desc:  "16 converted to XVI",
 			input: 16,
 			want:  "XVI",
 		},
 		{
-			desc:  "17 converted to XVII",
 			input: 17,
 			want:  "XVII",
 		},
 		{
-			desc:  "18 converted to XVIII",
 			input: 18,
 			want:  "XVIII",
 		},
 		{
-			desc:  "19 converted to XIX",
 			input: 19,
 			want:  "XIX",
 		},
 		{
-			desc:  "20 converted to XX",
 			input: 20,
 			want:  "XX",
 		},
 		{
-			desc:  "29 converted to XXIX",
 			input: 29,
 			want:  "XXIX",
 		},
 		{
-			desc:  "30 converted to XXX",
 			input: 30,
 			want:  "XXX",
 		},
 		{
-			desc:  "39 converted to XXXIX",
 			input: 39,
 			want:  "XXXIX",
 		},
 		{
-			desc:  "40 converted to XXXX",
+			input: 47,
+			want:  "XLVII",
+		},
+		{
 			input: 40,
-			want:  "XXXX",
+			want:  "XL",
+		},
+		{
+			input: 50,
+			want:  "L",
+		},
+		{
+			input: 99,
+			want:  "XCIX",
+		},
+		{
+			input: 100,
+			want:  "C",
+		},
+		{
+			input: 129,
+			want:  "CXXIX",
+		},
+		{
+			input: 150,
+			want:  "CL",
+		},
+		{
+			input: 498,
+			want:  "CDXCVIII",
+		},
+		{
+			input: 500,
+			want:  "D",
+		},
+		{
+			input: 1984,
+			want:  "MCMLXXXIV",
+		},
+		{
+			input: 1993,
+			want:  "MCMXCIII",
+		},
+		{
+			input: 4680,
+			want:  "MMMMDCLXXX",
+		},
+		{
+			input: 10000,
+			want:  "MMMMMMMMMM",
 		},
 	}
 	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d converted to %q\n", tC.input, tC.want), func(t *testing.T) {
 
 			got := ConvertToRoman(tC.input)
 
@@ -139,4 +165,21 @@ func TestRomanNumerals(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ExampleRomanNumberFifty() {
+	arabic := 50
+	got := ConvertToRoman(arabic)
+
+	fmt.Printf("arabic = %d , roman = %q", arabic, got)
+	//Output:
+	// arabic = 50 , roman = "L"
+}
+func ExampleRomanNumberFortyNine() {
+	arabic := 49
+	got := ConvertToRoman(arabic)
+
+	fmt.Printf("arabic = %d , roman = %q", arabic, got)
+	//Output:
+	// arabic = 49 , roman = "XLIX"
 }
