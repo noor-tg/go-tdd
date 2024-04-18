b624274d8a6b3b23b2ff8c1d04846b6579530560
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-18 20:44:57 +0200

    test(clockface):more clean tests
    - remove not needed style attrs in line, circle structs

--- clockface/clockface_acceptance_test.go
+++ clockface/clockface_acceptance_test.go
@@ -20,18 +20,16 @@ type SVG struct {
 }
 
 type Line struct {
-	X1    float64 `xml:"x1,attr"`
-	Y1    float64 `xml:"y1,attr"`
-	X2    float64 `xml:"x2,attr"`
-	Y2    float64 `xml:"y2,attr"`
-	Style string  `xml:"style,attr"`
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
 }
 
 type Circle struct {
-	Cx    float64 `xml:"cx,attr"`
-	Cy    float64 `xml:"cy,attr"`
-	R     float64 `xml:"r,attr"`
-	Style string  `xml:"style,attr"`
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
 }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
@@ -43,14 +41,13 @@ func TestSVGWriterAtMidnight(t *testing.T) {
 	svg := SVG{}
 	xml.Unmarshal(b.Bytes(), &svg)
 
-	x2 := 150.000
-	y2 := 60.000
+	want := Line{150, 150, 150, 60}
 
 	for _, line := range svg.Line {
-		if line.X2 == x2 && line.Y2 == y2 {
+		if line == want {
 			return
 		}
 	}
 
-	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", want.X2, want.Y2, b.String())
 }
