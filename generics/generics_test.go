a2c0e0dff4df40d8f40e9c61f55b08f245805706
Alnoor Altegany <alnoor.altegany@gmail.com>
2024-04-24 00:25:42 +0200

    feat(generics): learn and use generics to make testing assertion helpers

---
+++ generics/generics_test.go
@@ -0,0 +1,29 @@
+package generics_test
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on strings", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "Grace")
+	})
+}
+
+func AssertNotEqual[T comparable](t *testing.T, i1, i2 T) {
+	t.Helper()
+	if i1 == i2 {
+		t.Errorf("i1 %+v, i2 %+v", i1, i2)
+	}
+}
+
+func AssertEqual[T comparable](t *testing.T, i1, i2 T) {
+	t.Helper()
+	if i1 != i2 {
+		t.Errorf("i1 %+v, i2 %+v", i1, i2)
+	}
+}
